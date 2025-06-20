3f2c113e24e26729d0ad38b163cd841c227a303e
Loboda Nicolae <77647358+lylyss@users.noreply.github.com>
2025-06-20 14:50:27 +0200

    Aggiunta della gestione della luminosità nella classe Immagine

--- src/Multimedia/Immagine.java
+++ src/Multimedia/Immagine.java
@@ -1,9 +1,37 @@
 package Multimedia;
 
-// Classe base Immagine
-public class Immagine extends Multimedia {
+import Interface.Luminosita;
 
-    public Immagine(String titolo) {
+// Classe che rappresenta un'immagine con luminosità regolabile
+public class Immagine extends Multimedia implements Luminosita {
+    private int luminosita;
+
+    // Costruttore
+    public Immagine(String titolo, int luminosita) {
         super(titolo);
+        this.luminosita = Math.max(1, luminosita);
+    }
+
+    // Mostra l'immagine stampando il titolo seguito da asterischi
+    public void show() {
+        String asterischi = "*".repeat(luminosita);
+        System.out.println(titolo + asterischi);
+    }
+
+    // Aumenta la luminosità
+    @Override
+    public void aumentaLuminosita() {
+        luminosita++;
+    }
+
+    // Diminuisce la luminosità
+    @Override
+    public void diminuisciLuminosita() {
+        if (luminosita > 1) luminosita--;
+    }
+
+    // Getter per la luminosità
+    public int getLuminosita() {
+        return luminosita;
     }
 }
